f29f769bd237e37febfed033ebddc733796e5ee9
David M Vermillion <social@davidmvermillion.com>
2021-11-14 21:19:09 -0700

    Renamed script to match contents

--- GitHubConnection.R
+++ GitHubConnection.R
@@ -4,9 +4,9 @@
 # Credit to https://rfortherestofus.com/2021/02/how-to-use-git-github-with-r/
 
 
-install.packages('usethis, gitcreds')
-library('usethis, gitcreds')
-create_github_token() # to redirect to correct webpage
-gitcreds_set() # to insert token
+# install.packages('usethis, gitcreds')
+# library('usethis, gitcreds')
+# create_github_token() to redirect to correct webpage
+# gitcreds_set() to insert token
 
 library('tidyverse')
