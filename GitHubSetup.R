9390d631877874b51ff55f0c22ec158b664781a6
David M Vermillion <social@davidmvermillion.com>
2021-11-17 21:28:43 -0700

    Corrected multiple package loading methodology.

--- GitHubSetup.R
+++ GitHubSetup.R
@@ -1,7 +1,9 @@
 # Setting up Github using packages usethis and gitcreds
 # Credit to https://rfortherestofus.com/2021/02/how-to-use-git-github-with-r/
 
-install.packages('usethis, gitcreds') # not used in this form, not sure if concat required
-library('usethis, gitcreds')
+# Correction to methodology to load multiple packages at once: https://gist.github.com/RobertMyles/96665be044540e9ac668ee697dac6db7
+packages <- c("usethis", "gitcreds")
+lapply(packages, library, character.only = TRUE)
+
 create_github_token() # to redirect to correct webpage
 gitcreds_set() # to insert token
