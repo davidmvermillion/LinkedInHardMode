422ff45319ea76cbf1702204bd4b6d42c9da27c2
David M Vermillion <social@davidmvermillion.com>
2021-11-20 17:23:23 -0700

    Analysis Structure set

--- Analysis.R
+++ Analysis.R
@@ -4,11 +4,35 @@ library('tidyverse')
 
 # Load data into Tibble
 data <- read.csv("~/GitHub/LinkedInHardMode/LinkedInHardMode/LinkedInPostPerformancesDay25.csv")
+# Correct to most recent file before running script
 
 # Exploring weird read-in
+
+
+# Exploring weird read-in -------------------------------------------------
 summary(data)
 head(data)
 a <- data %>% select(contains("Day"))
 
-# Corrected Day column
-rename(data, Day = ï..Day)
+# Corrected Day column ----
+data <- rename(data, Day = ï..Day)
+head(data)
+plot(data)
+
+# Create useful selections ----
+CategoryCount <- count(data, Category)
+TypeCount <- count(data, Type)
+InteractionsCount <- count(data, Interactions)
+CommentsCount <- count(data, Comments)
+
+# Violin plot Views, Interactions, and Comments ----
+
+
+
+# Bar Graph Comparison of Views, Interactions, and Comments ---------------
+
+
+
+# Compare Categories and Post Types ---------------------------------------
+
+
