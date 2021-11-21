e5724da90d29b06cbce834993aa57289a9e4f09f
David M Vermillion <social@davidmvermillion.com>
2021-11-21 14:04:39 -0700

    Added experimentation files

---
+++ Bars.R
@@ -0,0 +1,2 @@
+# Figure out optimal chart setup for bar charts
+# Requires environment load from Analysis.R
---
+++ Violins.R
@@ -0,0 +1,7 @@
+# Figure out optimal chart setup for violin charts
+# Requires environment load from Analysis.R
+
+vp_v <- ggplot(data, aes(Day, Views)) + geom_violin()
+vp_v
+vp_vco <- ggplot(challengeonly, aes(Day, Views)) + geom_violin()
+vp_vco <- vp_vco + geom_violin(draw_quantiles = c(0.25, 0.5, 0.75))
