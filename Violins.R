f7c4d9280608e442bacf6c6099fc6303a07e55d1
David M Vermillion <social@davidmvermillion.com>
2021-11-21 14:08:40 -0700

    Update Violins.R
    Violin theme chosen

--- Violins.R
+++ Violins.R
@@ -1,7 +1,10 @@
 # Figure out optimal chart setup for violin charts
 # Requires environment load from Analysis.R
 
-vp_v <- ggplot(data, aes(Day, Views)) + geom_violin()
+vp_v <- data %>% 
+  ggplot(aes(Day, Views)) +
+  geom_violin(fill = "gray80") +
+  theme_minimal()
 vp_v
 vp_vco <- ggplot(challengeonly, aes(Day, Views)) + geom_violin()
 vp_vco <- vp_vco + geom_violin(draw_quantiles = c(0.25, 0.5, 0.75))
