eb83db245f3b41398632531b5f4e75052a57e82c
David M Vermillion <social@davidmvermillion.com>
2021-11-21 20:08:34 -0700

    Update Violins.R
    Rough method of violin plot made

--- Violins.R
+++ Violins.R
@@ -4,7 +4,19 @@
 vp_v <- data %>% 
   ggplot(aes(Day, Views)) +
   geom_violin(fill = "gray80") +
-  theme_minimal()
+  theme_minimal() +
+  ylab("View \n Counts") +
+  # geom_violin(draw_quantiles = c(0.25, 0.5, 0.75)) +
+  # Quantile doesn't work to overlay in this manner
+  theme(axis.title.y = element_text(angle = 0, vjust = 0, size = 20),
+        axis.title.x = element_blank(),
+        plot.title = element_text(hjust = 0.5, color = "grey55", size = 30),
+        axis.ticks = element_blank(),
+        axis.text.x = element_blank())
 vp_v
+
+
+
+# Not worked on yet
 vp_vco <- ggplot(challengeonly, aes(Day, Views)) + geom_violin()
 vp_vco <- vp_vco + geom_violin(draw_quantiles = c(0.25, 0.5, 0.75))
