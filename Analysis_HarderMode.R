f94f7f1889655c9a21dea9ba88004005ea71c2c8
David M Vermillion <social@davidmvermillion.com>
2022-02-03 14:29:03 -0700

    Import statements set up

--- Analysis_HarderMode.R
+++ Analysis_HarderMode.R
@@ -2,4 +2,14 @@
 
 library(tidyverse)
 
-Posts <- read.csv("LinkedInPostPerformancesDay98.csv", fileEncoding = "UTF-8-BOM")
+# Import
+posts <- read.csv("LinkedInPostPerformancesDay98.csv", fileEncoding = "UTF-8-BOM")
+profileviews <- read.csv("ProfileViews.csv", fileEncoding = "UTF-8-BOM")
+connections <- read.csv("ConnectionCountbyDate.csv", fileEncoding = "UTF-8-BOM")
+
+# Format
+posts$Date <- as.Date(posts$Date)
+profileviews$Weekof <- as.Date(profileviews$Weekof)
+connections$Date <- as.Date(connections$Date)
+
+plot(posts$Views)
